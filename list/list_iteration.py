65af7e49966d1d9518132358206a411a2746d7e7
bgamble43 <62437531+bgamble43@users.noreply.github.com>
2021-02-25 08:15:39 -0500

    List practice

---
+++ list/list_iteration.py
@@ -0,0 +1,14 @@
+list = [1001, 2002, 3003, 4004, "a", "e", "i", "o", "u" ]
+
+print("==============")
+print("for i in list")
+print("==============")
+for i in list:
+ print(f"i = {i}")
+
+print("")
+print("============================")
+print("for i in range(len(list))")
+print("============================")
+for i in range(len(list)):
+ print(f"i = {i}")
