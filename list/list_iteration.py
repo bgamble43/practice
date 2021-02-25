9202a1eec0fdaf44880172ee94e3f80fbabaf7d7
bgamble43 <62437531+bgamble43@users.noreply.github.com>
2021-02-25 08:22:01 -0500

    Updated code and readme

--- list/list_iteration.py
+++ list/list_iteration.py
@@ -1,14 +1,21 @@
 list = [1001, 2002, 3003, 4004, "a", "e", "i", "o", "u" ]
 
+print("====================")
+print("======= LIST =======")
+print("====================")
+print(list)
+print("")
+
 print("==============")
 print("for i in list")
 print("==============")
 for i in list:
  print(f"i = {i}")
-
 print("")
+
 print("============================")
 print("for i in range(len(list))")
 print("============================")
 for i in range(len(list)):
  print(f"i = {i}")
+print("")
